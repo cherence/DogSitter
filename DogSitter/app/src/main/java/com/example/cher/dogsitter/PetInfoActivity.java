0640b1db95605e67ae902e3c80655cc7a496a513
Cherence Flores <leisforkokomo@CHERENCE.local>
2016-05-09 11:04:20 -0700

    Fixed facebook login bug and added color scheme to colors resource file

--- DogSitter/app/src/main/java/com/example/cher/dogsitter/PetInfoActivity.java
+++ DogSitter/app/src/main/java/com/example/cher/dogsitter/PetInfoActivity.java
@@ -9,5 +9,7 @@ public class PetInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet_info);
+
+
     }
 }
