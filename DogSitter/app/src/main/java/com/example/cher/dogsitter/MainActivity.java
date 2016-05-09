e0c45b1e463168b03f248c6b241dbafcb1fb7652
Cherence Flores <leisforkokomo@CHERENCE.local>
2016-05-08 18:03:38 -0700

    Started on Facebook Login

--- DogSitter/app/src/main/java/com/example/cher/dogsitter/MainActivity.java
+++ DogSitter/app/src/main/java/com/example/cher/dogsitter/MainActivity.java
@@ -1,9 +1,24 @@
 package com.example.cher.dogsitter;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import com.facebook.FacebookSdk;
+
+import com.facebook.login.widget.LoginButton;
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private TextView mLoggedInStatusTextView;
+    private ProgressDialog mAuthProgressDialog;
+    private Firebase mFirebaseRef;
+    private AuthData mAuthData;
+    private Firebase.AuthStateListener mAuthStateListener;
+    private LoginButton mFacebookLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,3 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
+
+
+//app id 243406302682623
+//app secret 040b13edefb76aefd66f3c4416e2b95e
